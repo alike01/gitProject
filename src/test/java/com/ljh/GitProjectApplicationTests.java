2051e8b0cef65c83a4f135f4d9189b40a03c9136
ljh <2429683999@qq.com>
2023-01-25 16:54:39 +0800

    first

---
+++ src/test/java/com/ljh/GitProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ljh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjectApplicationTests {
+
+   @Test
+    public void testGit(){
+       System.out.println("hello,git");
+   }
+
+}
